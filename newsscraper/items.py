7dbcca4c553f3c2612644784daef88ce3c036bfa
maclipi <mayankanildwivedi@gmail.com>
2019-10-15 20:59:23 +0530

    Initial scrap

---
+++ newsscraper/items.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class NewsscraperItem(scrapy.Item):
+    # define the fields for your item here like:
+    headline = scrapy.Field()
+    colortext = scrapy.Field()
+    fullnews = scrapy.Field()

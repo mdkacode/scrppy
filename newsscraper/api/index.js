2ca67da60873bea76770a214f545be9914316583
hmm-mayank <mayankanildwivedi@gmail.com>
2019-11-21 21:33:26 +0530

    Babel Configuartion

---
+++ newsscraper/api/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const PORT = 8786;
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/',(req,res,next)=>{
+    console.log(`${req.url} ${req.method}`);
+    next();
+})
+;
+
+app.get('/news',(req,res,next)=>{
+    let data = req.query.cat;
+    let dd ='TV%20&%20Showbiz';
+   
+    console.log(decodeURI(req.query.cat))
+    
+    let rawdata = fs.readFileSync('../items.json');
+let student = JSON.parse(rawdata);
+let catData = [];
+if (data)
+{
+    student.map(res=>{
+        if(res.category == decodeURIComponent(data)){
+            catData.push(res);
+        }
+    })
+    res.jsonp(catData);
+}
+else {
+    res.jsonp(student);
+}
+
+})
+
+app.listen(PORT,console.log(`I am loving @ ${PORT}`));
